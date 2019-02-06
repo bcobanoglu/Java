974b64bfd009b25166884eab1cbc2e30e16b4938
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2019-02-06 22:10:56 +0300

    Create Ornek4_3.java

---
+++ Bolum4/Ornek4_3.java
@@ -0,0 +1,15 @@
+//Boolean aritmetiği
+public class Ornek4_3 {
+  public static void main(String[] args) {
+	 boolean b1=true;
+	 boolean b2=false;
+	 System.out.println("b1..:" + b1);
+	 System.out.println("b2..:" + b2);
+  }
+}
+
+/*Output:
+b1..:true
+b2..:false
+
+*/

270c4abd7ecfe471c90b30e3113bd8b3aa7066fe
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2019-02-06 22:03:40 +0300

    Create Ornek4_1.java

---
+++ Bolum4/Ornek4_1.java
@@ -0,0 +1,14 @@
+//Örnek 4.1. MIN_VALUE/ MAX_VALUE ve SIZE komutlarının kullanım örneği
+System.out.println("int maksimumu=" + Integer.MAX_VALUE );
+System.out.println("float maksimumu=" + Float.MAX_VALUE); 
+System.out.println("byte minimumu=" + Byte.MIN_VALUE);   
+System.out.println("byte kapasitesi=" + Byte.SIZE +" bit");   
+System.out.println("int kapasitesi=" + Integer.SIZE +" bit");
+
+/*Output:
+int maksimumu=2147483647
+float maksimumu=3.4028235E38
+byte minimumu=-128
+byte kapasitesi=8 bit
+int kapasitesi=32 bit
+*/
